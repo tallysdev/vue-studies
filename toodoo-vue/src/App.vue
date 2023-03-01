1cc87e5ee8444703ce8be8d95fa41767e323fd16
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-01 17:59:45 -0300

    :sparkles:feat: Integration with component

--- toodoo-vue/src/App.vue
+++ toodoo-vue/src/App.vue
@@ -1,11 +1,18 @@
 <template lang="">
   <div>
-    
+    <h1>Tallys</h1>
   </div>
+  <MyFirstComponents/>
 </template>
 <script>
+
+import MyFirstComponents from './components/MyFirstComponents.vue';
+
 export default {
-  
+  name: 'App',
+  components: {
+    MyFirstComponents
+  }
 }
 </script>
 <style lang="">
