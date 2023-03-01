c6f156789e032b4d39cd5379b0266ce14f7792b6
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-01 17:56:04 -0300

    :sparkles:feat: My Firts Component

---
+++ toodoo-vue/src/components/MyFirstComponents.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Aureliano {{ nome }} e sou {{profissao}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyFirstComponents',
+    data() {
+        return {
+            nome: "Dantas dos Santos",
+            profissao: "Programandor FullStack"
+        }
+    },
+}
+</script>
+<style scoped>
+
+</style>
