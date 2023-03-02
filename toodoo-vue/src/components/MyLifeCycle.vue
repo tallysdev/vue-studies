ee1c4c128bd2f30074f83f13c9c83b986311b4e9
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-01 21:14:07 -0300

    :sparkles:feat: a little lifecycle hooks

---
+++ toodoo-vue/src/components/MyLifeCycle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Meu nome é: {{ name }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+            name: 'Nome ainda nao sei'
+        }
+    },
+    created(){
+        setTimeout(() => {
+            this.name = "Jose"
+        }, 1000);
+    },
+    mounted() {
+        this.name = "tallys"
+    },
+}
+</script>
+
+<style scoped>
+    
+</style>
