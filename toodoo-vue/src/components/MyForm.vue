74ba541bed7edf5ce6d28c605e2e08d34a4a37c3
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-12 21:44:44 -0300

    :sparkles:feat: Events

--- toodoo-vue/src/components/MyForm.vue
+++ toodoo-vue/src/components/MyForm.vue
@@ -1,10 +1,12 @@
 <template lang="">
     <div>
-        <form action="">
-            <MyInput/>
+        <form action="" @MySubmit="enviarFormulario($event)">
+            <input type="text" :v-model="name">
             <MyInput/>
             <MySubmit/>
         </form>    
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit deserunt, quae numquam nihil iste, vitae reprehenderit itaque sequi tempore beatae mollitia animi ea doloribus molestias. Incidunt perspiciatis cumque beatae iusto!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia tempore quod illum odio id veritatis sed, aut minima, quisquam cupiditate nihil reiciendis quae porro magnam incidunt? Aliquam voluptas praesentium vero.</p>
     </div>
 </template>
 
@@ -19,13 +21,26 @@ export default {
         MyInput,
         MySubmit
     },
+    methods: {
+        enviarFormulario(e){
+            e.preventDefault();
+
+            const name = this.name;
+            console.log(name);
+
+            // Ajax
+            
+        }
+    },
     data() {
         return {
-            
+            name: "tallys",
         }
     },
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    p {
+        margin: 25px;
+    }
 </style>
