f5c12417c89d6a0654ecef2f5ea29649854ac5cb
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-13 20:35:35 -0300

    :sparkles:feat: Multiplus events

--- toodoo-vue/src/components/MyFirstComponents.vue
+++ toodoo-vue/src/components/MyFirstComponents.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
         <h2>Aureliano {{ nome }} e sou {{profissao}}</h2>
+        <MyMultiplosEventos/>
     </div>
+    
 </template>
 
 <script>
+
+import MyMultiplosEventos from './MyMultiplosEventos.vue';
+
 export default {
     name: 'MyFirstComponents',
+    components: {
+        MyMultiplosEventos,
+    },
     data() {
         return {
             nome: "Dantas dos Santos",
---
+++ toodoo-vue/src/components/MyMultiplosEventos.vue
@@ -0,0 +1,38 @@
+<template lang="">
+    <div>
+        <button @click="primeiro(txt1,$event), segundo(txt2,$event),terceiro()">Ativar MyMultiplos Eventos</button>
+        <p>{{multiplosEventos}}</p>
+    </div>
+</template>
+<script>
+
+export default {
+    name: "MyMultiplosEventos",
+    data() {
+        return {
+            multiplosEventos: "",
+            txt1: "Priemeiro evento",
+            txt2: "Segundo evento"
+        }
+    },
+    methods: {
+        // eslint-disable-next-line no-unused-vars
+        primeiro(txt, e) {
+            this.multiplosEventos = txt
+        },
+        // eslint-disable-next-line no-unused-vars
+        segundo(txt, e) {
+            setTimeout(() => this.multiplosEventos = txt, 500)
+        },
+        terceiro() {
+            setTimeout(() => this.multiplosEventos = "", 1500)
+
+        },
+    },
+}
+</script>
+<style scoped>
+div {
+    margin: 100px;
+}
+</style>
