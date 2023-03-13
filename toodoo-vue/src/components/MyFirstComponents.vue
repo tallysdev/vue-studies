b2af04799abfc9ad3de17887275cb1754058cecc
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-13 20:59:51 -0300

    :sparkles:feat: Component reuse

--- toodoo-vue/src/components/MyFirstComponents.vue
+++ toodoo-vue/src/components/MyFirstComponents.vue
@@ -2,6 +2,11 @@
     <div>
         <h2>Aureliano {{ nome }} e sou {{profissao}}</h2>
         <MyMultiplosEventos/>
+        <MyReuse/>
+        <MyReuse/>
+        <MyReuse/>
+        <MyReuse/>
+        
     </div>
     
 </template>
@@ -9,11 +14,13 @@
 <script>
 
 import MyMultiplosEventos from './MyMultiplosEventos.vue';
+import MyReuse from './MyReuse.vue';
 
 export default {
     name: 'MyFirstComponents',
     components: {
         MyMultiplosEventos,
+        MyReuse,
     },
     data() {
         return {
---
+++ toodoo-vue/src/components/MyReuse.vue
@@ -0,0 +1,25 @@
+<template lang="">
+    <div>
+        <button @click="ativar">Clique aqui</button>
+        <span v-show="ativou"> Ativado!</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"MyReuse",
+    data() {
+        return {
+            ativou: true,
+        }
+    },
+    methods: {
+        ativar(){
+            this.ativou = !this.ativou
+        }
+    },
+}
+</script>
+<style scoped>
+
+</style>
