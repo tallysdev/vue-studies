a0da61e14e5154f8558abc32bd0eaca789ce3f4d
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-13 22:11:53 -0300

    :sparkles:feat: Emit

---
+++ toodoo-vue/src/components/MyChangeImage.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <div>
+        <button @click="$emit('MudarImg')">trocar img</button> 
+    </div>
+</template>
+<script>
+
+export default {
+    name: "MyChangeImage",
+    emits: ['MudarImg'],
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+}
+</script>
+<style lang="">
+    
+</style>
--- toodoo-vue/src/components/MyPicture.vue
+++ toodoo-vue/src/components/MyPicture.vue
@@ -1,18 +1,33 @@
 <template lang="">
     <img :src="avatar" :alt="descricao ">
+    <MyChangeImage @mudar-img=" trocarImg"/>
 </template> 
 
 <script>
+
+import MyChangeImage from './MyChangeImage.vue';
+
 export default {
     name: "MyPicture",
+    components: {
+        MyChangeImage,
+    },
     data() {
         return {
-            avatar: "favicon.ico",
+            avatar: "/favicon.ico",
             descricao: "minha foto",
         }
     },
+    methods: {
+        trocarImg() {
+            this.avatar = "/coca-cola-logo-2007.jpg"
+        },
+    },
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    img {
+        max-width: 100px;
+        max-height: 50px;
+    }
 </style>
