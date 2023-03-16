a8488c0934509b76e95f124bca5fa1478c6c05d9
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-16 17:08:10 -0300

    fix: router link error

---
+++ myproject-vue/src/components/MyFooter.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+        <footer>
+            Todos os direitos reservados
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'MyFooter'
+}
+</script>
+<style scoped>
+    
+</style>
---
+++ myproject-vue/src/components/MyNavBar.vue
@@ -0,0 +1,14 @@
+<template >
+    <div>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/MyPedidos">Pedidos</router-link>
+    </div>
+</template>
+<script>
+export default {
+    name:'MyNavBar'
+}
+</script>
+<style scoped>
+    
+</style>
