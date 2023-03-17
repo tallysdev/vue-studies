e0c5d8653a326b97b7aa089a446ed6c2257f552b
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-16 21:46:55 -0300

    :sparkles:feat: Global styling and common classes

--- myproject-vue/src/App.vue
+++ myproject-vue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <MyNavBar/>
+    <MyNavBar :logo="logo_src" :alt="app_name"/>
     <router-view/>
     <MyFooter/>
   </section>
@@ -15,10 +15,35 @@ export default {
   components: {
     MyNavBar,
     MyFooter,
-  }
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger",
+    }
+  },
 }
 </script>
 
 <style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .main-container {
+    margin: 50px;
+    min-height: 435px; 
+    /* min-height: 250PX; */
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 
 </style>
