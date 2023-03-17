fad4394690b3c031020151e2f5f88940160bbd6c
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-16 21:58:40 -0300

    :sparkles:feat: Body and footer styling

--- myproject-vue/src/components/MyFooter.vue
+++ myproject-vue/src/components/MyFooter.vue
@@ -1,16 +1,24 @@
 <template lang="">
-    <div>
-        <footer>
-            Todos os direitos reservados
-        </footer>
-    </div>
+    <footer id="footer">
+        <p>Make Your Burger &copy; 2023</p>
+    </footer>
 </template>
 
 <script>
 export default {
-    name:'MyFooter'
+    name: 'MyFooter'
 }
 </script>
+
 <style scoped>
-    
+    #footer {
+        height: 150px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
 </style>
