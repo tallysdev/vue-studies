54d647cffc79adfacd4462cbdc5963d086c7f309
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-18 20:59:59 -0300

    :sparkles:feat: Creating async function to add hamburger

--- myproject-vue/src/components/MyBurgerForm.vue
+++ myproject-vue/src/components/MyBurgerForm.vue
@@ -2,7 +2,7 @@
     <div>
     <p>Componente de Mensagem</p>
     <div>
-        <form id="burger-form">
+        <form id="burger-form" @submit="createBurger">
             <div class="input-container">
                 <label for="nome">Nome do cliente:</label>
                 <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
@@ -48,7 +48,6 @@ export default {
             pao: null,
             carne: null,
             opcionais: [],
-            status: "Solicitado",
             msg: null
         }
     },
@@ -60,8 +59,40 @@ export default {
             this.paes = data.paes;
             this.carnes = data.carnes;
             this.opcionaisdata = data.opcionais;
+        },
+
+        async createBurger(e){
+        e.preventDefault();
+        console.log(e);
+        const data = 
+        {
+            nome: this.nome,
+            carne: this.carne,
+            pao: this.pao,
+            opcionais: Array.from(this.opcionais),
+            status: "Solicitado"
         }
+
+        const dataJson = JSON.stringify(data);
+
+        const req = await fetch("http://localhost:3000/burgers", {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: dataJson
+    });
+        const res = await req.json();
+
+        console.log(res);
+
+        this.nome = "",
+        this.pao = "",
+        this.carme = "",
+        this.opcionais = ""
+
     },
+    },
+
+
     mounted() {
         this.getIngredientes();
     },
