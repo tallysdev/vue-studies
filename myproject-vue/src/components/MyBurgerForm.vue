0b120b6a5053adb6535bf9c5b69ee870fbf34f90
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-18 20:17:52 -0300

    :sparkles:feat: Connecting to a database and creating database access variables

--- myproject-vue/src/components/MyBurgerForm.vue
+++ myproject-vue/src/components/MyBurgerForm.vue
@@ -11,29 +11,21 @@
                 <label for="pao">Escolha o seu Pão:</label>
                 <select name="pao" id="pao" v-model="pao">
                     <option value="">Selecione o seu pão</option>
-                    <option value="integral">Integral</option>
+                    <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{ pao.tipo }}</option>
                 </select>
             </div>
             <div class="input-container">
                 <label for="carne">Escolha a sua Carne:</label>
                 <select name="carne" id="carne" v-model="carne">
                     <option value="">Selecione a sua carne</option>
-                    <option value="porco">porco</option>
+                    <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{ carne.tipo }}</option>
                 </select>
             </div>
             <div id="opcionais-container" class="input-container">
                 <label id="opcionais-tilte" for="opcionais">Selecione os opcionais:</label>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="Salame">
-                    <span>Salame</span>
-                </div>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="Salame">
-                    <span>Salame</span>
-                </div>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="Salame">
-                    <span>Salame</span>
+                <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
+                    <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                    <span>{{ opcional.tipo }}</span>
                 </div>
                 <div class="input-container">
                     <input type="submit" value="Criar meu Burger" class="submit-btn">
@@ -46,7 +38,33 @@
 
 <script>
 export default {
-    name: "MyBurgerForm"
+    name: "MyBurgerForm",
+    data() {
+        return {
+            paes: null,
+            carnes: null,
+            opcionaisdata: null,
+            nome:null,
+            pao: null,
+            carne: null,
+            opcionais: [],
+            status: "Solicitado",
+            msg: null
+        }
+    },
+    methods: {
+        async getIngredientes(){
+            const req = await fetch("http://localhost:3000/ingredientes");
+            const data = await req.json();
+
+            this.paes = data.paes;
+            this.carnes = data.carnes;
+            this.opcionaisdata = data.opcionais;
+        }
+    },
+    mounted() {
+        this.getIngredientes();
+    },
 }
 </script>
 <style scoped>
