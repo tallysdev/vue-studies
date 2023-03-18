89350f93709427795bc9c081367d4343ec528ade
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-18 18:35:01 -0300

    :sparkles:feat: Aded Banner component

--- myproject-vue/src/views/MyHome.vue
+++ myproject-vue/src/views/MyHome.vue
@@ -1,13 +1,17 @@
 <template>
-    <p>banner</p>
+    <MyBanner/>
     <div class="main-container">
         <h1>Monte o seu Burger</h1>
         <p>Formulario</p>
     </div>
 </template>
 <script>
+import MyBanner from '@/components/MyBanner.vue';
 export default {
     name:"MyHome",
+    components: {
+        MyBanner,
+    }
 }
 </script>
 <style scoped>
