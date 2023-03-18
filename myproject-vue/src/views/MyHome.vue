c00e478698f67d402f8fa8c6f3468d472de2ec6c
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-18 19:29:20 -0300

    :sparkles:feat: Adding BurgerForm component to home

--- myproject-vue/src/views/MyHome.vue
+++ myproject-vue/src/views/MyHome.vue
@@ -2,15 +2,19 @@
     <MyBanner/>
     <div class="main-container">
         <h1>Monte o seu Burger</h1>
-        <p>Formulario</p>
+        <MyBurgerForm/>
     </div>
 </template>
 <script>
+
 import MyBanner from '@/components/MyBanner.vue';
+import MyBurgerForm from '@/components/MyBurgerForm.vue';
+
 export default {
     name:"MyHome",
     components: {
         MyBanner,
+        MyBurgerForm,
     }
 }
 </script>
