c02397c850cb7c777fbc25f231e096e618968023
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-18 18:33:32 -0300

    :sparkles:feat: Website banner section

---
+++ myproject-vue/src/components/MyBanner.vue
@@ -0,0 +1,35 @@
+<template lang="">
+    <div id="main-banner">
+        <h1>Make Your Burger</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyBanner',
+
+}
+</script>
+
+<style scoped>
+    
+    #main-banner {
+        background-image: url(../../public/img/burger.jpg);
+        background-position: 0 -250px; 
+        background-size: cover;
+        height: 600px;
+        /* height: 500px; */
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    #main-banner h1 {
+        color: white;
+        text-align: center;
+        font-size: 60px;
+        background-color: #222;
+        padding: 20px 40px;
+    }
+
+</style>
