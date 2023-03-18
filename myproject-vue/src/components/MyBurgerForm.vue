ebb8a2e6b4d44c63c4342824ea27945002e76a57
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-18 19:28:15 -0300

    :sparkles:feat: Implementing the form's HTML and CSS

---
+++ myproject-vue/src/components/MyBurgerForm.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+    <p>Componente de Mensagem</p>
+    <div>
+        <form id="burger-form">
+            <div class="input-container">
+                <label for="nome">Nome do cliente:</label>
+                <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
+            </div>
+            <div class="input-container">
+                <label for="pao">Escolha o seu Pão:</label>
+                <select name="pao" id="pao" v-model="pao">
+                    <option value="">Selecione o seu pão</option>
+                    <option value="integral">Integral</option>
+                </select>
+            </div>
+            <div class="input-container">
+                <label for="carne">Escolha a sua Carne:</label>
+                <select name="carne" id="carne" v-model="carne">
+                    <option value="">Selecione a sua carne</option>
+                    <option value="porco">porco</option>
+                </select>
+            </div>
+            <div id="opcionais-container" class="input-container">
+                <label id="opcionais-tilte" for="opcionais">Selecione os opcionais:</label>
+                <div class="checkbox-container">
+                    <input type="checkbox" name="opcionais" v-model="opcionais" value="Salame">
+                    <span>Salame</span>
+                </div>
+                <div class="checkbox-container">
+                    <input type="checkbox" name="opcionais" v-model="opcionais" value="Salame">
+                    <span>Salame</span>
+                </div>
+                <div class="checkbox-container">
+                    <input type="checkbox" name="opcionais" v-model="opcionais" value="Salame">
+                    <span>Salame</span>
+                </div>
+                <div class="input-container">
+                    <input type="submit" value="Criar meu Burger" class="submit-btn">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "MyBurgerForm"
+}
+</script>
+<style scoped>
+    #burger-form { 
+        max-width: 400px;
+        margin: 0 auto;
+    }
+
+    .input-container {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    label {
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: #222;
+        padding: 5px 10px;
+        border-left: 4px solid #FCBA03;
+    }
+
+    input, select {
+        padding: 5px 10px;
+        width: 300px;
+    }
+
+    #opcionais-container {
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    #opcionais-tilte {
+        width: 100%;
+    }
+
+    .checkbox-container {
+        display: flex;
+        align-items: flex-start;
+        width: 50%;
+        margin-bottom: 20px;
+    }
+
+    .checkbox-container span, .checkbox-container input {
+        width: auto;
+    }
+
+    .checkbox-container span {
+        margin-left: 6px;
+        font-weight: bold;
+    }
+
+    .submit-btn {
+        background-color: #222;
+        color: #FCBA03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: 0.5s;
+    }
+
+    .submit-btn:hover {
+        background-color: transparent;
+        color: #222;
+    }
+
+</style>
