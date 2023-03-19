67f73a355a3835a744f7dabb047bc2f6145e760f
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-18 22:02:34 -0300

    :sparkles:feat: Creating message component

---
+++ myproject-vue/src/components/MyMensage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="mensagem-container">
+        <p>{{ msg }}</p>
+    </div>
+</template>
+<script>
+
+export default {
+    name: "MyMensage",
+    props: {
+        msg: String,
+    },
+    data() {
+        return {
+            
+        }
+    },    
+}
+</script>
+<style scoped>
+    .mensagem-container {
+        color: #004085;
+        /* text-align: center; */
+        background-color: #CCE5FF;
+        border: 2px solid #B8DAFF;
+        border-radius: 5px;
+        padding: 10ox;
+        max-width: 400px;
+        margin: 30px auto;
+    }
+</style>
