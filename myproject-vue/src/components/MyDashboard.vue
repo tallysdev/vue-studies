476bebd291c4c2cf6e6d264337ceab5db45e9be0
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-19 13:45:47 -0300

    :sparkles:feat: Connecting attributes with database

--- myproject-vue/src/components/MyDashboard.vue
+++ myproject-vue/src/components/MyDashboard.vue
@@ -11,15 +11,14 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>Joao</div>
-                <div>Pao de trigo</div>
-                <div>Maminha</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{ burger.nome }}</div>
+                <div>{{ burger.pao }}</div>
+                <div>{{ burger.carne }}</div>
                 <div>
                     <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">{{ opcional }}</li>
                     </ul>
                 </div>
                 <div>
@@ -35,9 +34,30 @@
 
 <script>
 export default {
-    name: "MyDeshboard"
+    name: "MyDeshboard",
+    data() {
+        return {
+            burgers: null,
+            burgers_id: null,
+            status: []
+        }
+    },
+    methods: {
+        async getPedidos() {
+
+            const req = await fetch("http://localhost:3000/burgers");
+
+            const data = await req.json();
+
+            this.burgers = data;
+        }
+    },
+    mounted() {
+        this.getPedidos();
+    },
 }
 </script>
+
 <style scoped>
     #burger-table {
         max-width: 1200px;
